ef0ce55bd8d303f9d2acc4229c3423273946ce87
AAswordman <1002153674@qq.com>
2026-03-19 18:36:30 +0800

    +1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 40
-        versionName = "1.10.0"
+        versionName = "1.10.0+1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
